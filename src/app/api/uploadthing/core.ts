3b403936e7dfd8867038cb5a6789583e6b98ee82
abdellah <Abdellah.chehri14@gmail.com>
2023-12-20 14:52:18 +0100

    push to main

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,11 +3,11 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "128MB" } }).onUploadComplete(
-    async ({ file }) => {
-      console.log("file url", file.url);
-    }
-  ),
+  imageUploader: f({
+    image: { maxFileSize: "128MB", maxFileCount: 10 },
+  }).onUploadComplete(async ({ file }) => {
+    console.log("file url", file.url);
+  }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
