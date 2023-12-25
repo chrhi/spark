f112833a10e81ee34df22aa4529763ae1d3dc2f4
abdellah <Abdellah.chehri14@gmail.com>
2023-12-25 05:55:42 +0100

    push to main

--- src/app/(store)/order-product/page.tsx
+++ src/app/(store)/order-product/page.tsx
@@ -1,18 +1,12 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { OrderForm } from "@/components/forms/order-form";
-import type { FC } from "react";
-import YourBagShowCase from "../_components/YourBagShowCase";
+import { FC } from "react";
 
 const page: FC = ({}) => {
   return (
     <MaxWidthWrapper>
       <div className="w-full min-h-screen h-fit grid grid-cols-2 ">
-        <div className="w-full my-8 rounded-xl h-[80%]  bg-gray-50">
-          <YourBagShowCase />
-        </div>
-        <div className="w-full h-full  p-6">
-          <OrderForm />
-        </div>
+        <div className="w-full my-8 rounded-xl h-[80%]  bg-gray-50"></div>
+        <div className="w-full h-full  p-6"></div>
       </div>
     </MaxWidthWrapper>
   );
