6bccb263c77a6fc5f445622db619515d902f5ba7
abdellah <Abdellah.chehri14@gmail.com>
2023-12-27 22:19:02 +0100

    push to main

--- src/app/(store)/_components/footer.tsx
+++ src/app/(store)/_components/footer.tsx
@@ -7,29 +7,29 @@ interface FooterAbdullahProps {}
 
 const Footer: FC = ({}) => {
   return (
-    <div className="w-full h-fit  overflow-hidden bg-black ">
+    <div className="w-full h-fit  mt-12 overflow-hidden bg-black ">
       <MaxWidthWrapper className="flex flex-col">
-        <div className="w-full h-full grid grid-cols-1 lg:grid-cols-3 p-8 ">
+        <div className="w-full h-full grid grid-cols-1 lg:grid-cols-3 p-8 gap-6 md:gap-0 ">
           <div className="w-full h-[150px] flex gap-y-4  flex-col justify-start items-start">
             <h4 className="text-white text-lg font-semibold">Quick links</h4>
-            <p className="text-white text-md">Bags</p>
-            <p className="text-white text-md">Shoes</p>
-            <p className="text-white text-md">Lookbook</p>
+            <p className="text-gray-100 text-md">facebook</p>
+            <p className="text-gray-100 text-md">instagram</p>
+            <p className="text-gray-100 text-md">tik tok</p>
           </div>
           <div className="w-full h-[150px] flex gap-y-4  flex-col justify-start items-start">
             <h4 className="text-white text-lg font-semibold">Info</h4>
-            <p className="text-white text-md">About</p>
-            <p className="text-white text-md">Contact us</p>
-            <p className="text-white text-md">Shipping policy</p>
+            <p className="text-gray-100 text-md">About</p>
+            <p className="text-gray-100 text-md">Contact us</p>
+            <p className="text-gray-100 text-md">Shipping policy</p>
           </div>
-          <div className="w-full h-[150px] gap-y-4 flex flex-col justify-start items-start">
+          <div className="w-full h-[150px] gap-y-4 flex flex-col justify-start  items-start">
             <h4 className="text-white text-lg font-semibold">Our mission</h4>
-            <p className="text-white text-md">
+            <p className="text-gray-100 text-md">
               Quality materials, good designs, craftsmanship and sustainability.
             </p>
           </div>
         </div>
-        <div className="w-full  h-[100px] flex justify-between items-end p-6 ">
+        <div className="w-full  h-[100px] flex  flex-col md:flex-row items-center  md:justify-between  p-6 ">
           <p className="text-white text-start">© 2023, All Rights resolved</p>{" "}
           <Secret />
         </div>
--- src/app/(store)/_components/secret.tsx
+++ src/app/(store)/_components/secret.tsx
@@ -24,49 +24,58 @@ import {
 } from "@/components/ui/drawer";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useIsMobile } from "@/hooks/use-is-mobile";
 
 export default function Secret() {
   const [open, setOpen] = React.useState(false);
 
-  return (
-    <>
+  const isMobil = useIsMobile();
+
+  if (!isMobil) {
+    return (
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
-          <Button variant="outline">Edit Profile</Button>
+          <Button variant="ghost" className="text-white">
+            site woner ? log in
+          </Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
+            <DialogTitle>Admin dashboard</DialogTitle>
             <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
+              Get Access to your admin dashboard so you can edit upload new
+              products into the store.
             </DialogDescription>
           </DialogHeader>
           <ProfileForm />
         </DialogContent>
       </Dialog>
+    );
+  }
 
-      <Drawer open={open} onOpenChange={setOpen}>
-        <DrawerTrigger asChild>
-          <Button variant="outline">Edit Profile</Button>
-        </DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader className="text-left">
-            <DrawerTitle>Edit profile</DrawerTitle>
-            <DrawerDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DrawerDescription>
-          </DrawerHeader>
-          <ProfileForm className="px-4" />
-          <DrawerFooter className="pt-2">
-            <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </>
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant="ghost" className="text-white">
+          site woner ? log in
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>Admin dashboard</DrawerTitle>
+          <DrawerDescription>
+            Get Access to your admin dashboard so you can edit upload new
+            products into the store.
+          </DrawerDescription>
+        </DrawerHeader>
+        <ProfileForm className="px-4" />
+        <DrawerFooter className="pt-2">
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }
 
