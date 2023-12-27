47ba8a22e37d9220dd0503bf47f0e731a6310b02
abdellah <Abdellah.chehri14@gmail.com>
2023-12-27 17:45:04 +0100

    push to main

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,10 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Banner from "./_components/Banner";
-import ProductsLine from "./_components/products-line";
-import ProductListing from "./_components/ProductListing";
 import { Product } from "@prisma/client";
 import { db } from "@/lib/db";
-import { StoredFile } from "@/types";
 import { ProdoctsCarousel } from "./_components/products-carousel";
 
 async function getData(): Promise<Product[]> {
@@ -19,8 +16,8 @@ export default async function Home() {
   return (
     <>
       <Banner />
-      <MaxWidthWrapper className="max-w-screen-2xl">
-        <div className="my-8 w-full h-[200px] flex flex-col justify-center items-start gap-y-4">
+      <MaxWidthWrapper className="max-w-screen-2xl px-4">
+        <div className="my-8 mt-14 w-full h-[200px] flex flex-col justify-center items-start gap-y-4">
           <h1 className="text-2xl mt-2 lg:text-5xl font-bold  text-start text-black">
             Obsessive Attention. Intelligent Effort.
           </h1>
